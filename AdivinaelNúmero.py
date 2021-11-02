09487412c99825a8b7863713744f13126091bfb7
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-02 10:56:50 +0100

    Update AdivinaelNúmero.py

--- AdivinaelNúmero.py
+++ AdivinaelNúmero.py
@@ -2,4 +2,8 @@ import random
 naleatorio = random.randint(0, 100)
 nelegido = input(print("Introduce el número generado: "))
 while nelegido != naleatorio:
-    
+    print("El número elegido no es el correcto")
+    if nelegido < naleatorio:
+        print("El número generado es menor al introducido")
+    else:
+        print("El número generado es mayor al introducido")
