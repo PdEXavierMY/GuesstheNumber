d5f4eb39fac619c6222c2df31aae5f3466b5d0e1
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-05 19:52:10 +0100

    "main"

--- AdivinaelNúmero.py
+++ AdivinaelNúmero.py
@@ -1,10 +1,15 @@
 import random
 print("Se va a generar un número aleatorio entero de entre 0 y 99.")
 naleatorio = random.randint(0, 99)
-nelegido = int(input("Introduce el número que crees que se ha generado: "))
-if nelegido == SyntaxError:
-    print("Por favor, introduzca un número entero.")
-    nelegido = int(input("Introduce el número que crees que se ha generado: "))
+while True:
+    nelegido = input("Introduce el número que crees que se ha generado: ")
+    try:
+        nelegido = int(nelegido)
+    except:
+        print("Por favor, introduce un número entero del 0 al 99")
+    else:
+        if 0 <= nelegido <= 99:
+            break
 intentos = 1
 while nelegido != naleatorio:
     print("El número elegido no es el correcto.")
@@ -14,7 +19,15 @@ while nelegido != naleatorio:
         print("El número generado es menor al introducido.")
     print("Vuelve a intentarlo.")
     intentos += 1
-    nelegido = int(input("Introduce el número que crees que se ha generado: "))
+    while True:
+        nelegido = input("Introduce el número que crees que se ha generado: ")
+        try:
+            nelegido = int(nelegido)
+        except:
+            print("Por favor, introduce un número entero del 0 al 99")
+        else:
+            if 0 <= nelegido <= 99:
+                break
 if nelegido == naleatorio:
     print("Has acertado!!!")
     print("El número de intentos que has necesitado es " + str(intentos))
