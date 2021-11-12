943287964bc1086e025eb826546d737b98622fd1
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-12 12:06:34 +0100

    Update AdivinaelNúmeroDefinitivo.py

--- AdivinaelNúmeroDefinitivo.py
+++ AdivinaelNúmeroDefinitivo.py
@@ -11,36 +11,15 @@ def pedir_numero():
         else: 
             if 9 <= entrada <= 99: 
                 break 
-    return entrada 
+    return entrada
 
-print("Introduzca el número a adivinar")
-while True:
-    numero = input("Introduzca un número entre 0 y 99 incluídos: ")
-    try:
-        numero = int(numero)
-    except:
-        pass
-    else:
-        if 0 <= numero <= 99:
-            break
-
-print("intente encontrar el número a adivinar")
-while True:
-    while True:
-        intento = input("Introduzca un número entre 0 y 99 incluídos: ")
-
-        try:
-            intento = int(intento)
-        except:
-            pass
-        else:
-            if 0 <= intento <= 99:
-                break
-
-    if intento < numero:
-        print("Demasiado pequeño")
-    elif intento > numero:
-        print("Demasiado grande")
-    else:
-        print("Victoria!")
+print("Intente adivinar el número") 
+while True: 
+    intento = pedir_numero() 
+    if intento < numero: 
+        print("Demasiado pequeño") 
+    elif intento > numero: 
+        print("Demasiado grande") 
+    else: 
+        print ("¡Ha ganado!")
         break
