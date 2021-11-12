bfe031892bbf4cbdfe05fac87c48239e9b6afe35
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-12 12:23:59 +0100

    Update AdivinaelNúmeroDefinitivo.py

--- AdivinaelNúmeroDefinitivo.py
+++ AdivinaelNúmeroDefinitivo.py
@@ -1,3 +1,15 @@
 import random
 print("Se va a generar un número aleatorio entero de entre 0 y 99.")
 numero = random.randint(0, 99)
+
+def pedir_numero(): 
+    while True: 
+        entrada = input("Introduzca un número entre 0 y 99: ") 
+        try: 
+            entrada = int(entrada) 
+        except: 
+            pass 
+        else: 
+            if 9 <= entrada <= 99: 
+                break 
+    return entrada 
