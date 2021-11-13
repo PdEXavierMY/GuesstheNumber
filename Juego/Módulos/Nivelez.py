87a1ccb8d4bc023dc68117d643303e2fcc2c7c46
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-13 19:34:42 +0100

    Update Nivelez.py

--- Juego/Módulos/Nivelez.py
+++ Juego/Módulos/Nivelez.py
@@ -1,3 +1,9 @@
+from Juego.Módulos.pedir_números import pedir_entrada_numero_delimitado
+
+
+from .pedir_números import (pedir_entrada_numero, pedir_entrada_numero_delimitado)
+
+
 def nivel1():
     minimo = 0
     maximo = 99
@@ -19,4 +25,6 @@ def nivel4():
     return minimo, maximo
 
 def elegirnivel():
-    input
+    print("Hay cuatro niveles de dificultad: 1, 2, 3, 4, aumentando la dificultad del juego cuanto mayor sea el número escogido")
+    pedir_entrada_numero_delimitado("Intoduzca el nivel que quiere jugar", 1, 4)
+elegirnivel()
