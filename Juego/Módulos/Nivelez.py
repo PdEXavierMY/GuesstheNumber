01c90dd68910cf6d35fc6bc26e526232acae1d4b
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-13 19:29:52 +0100

    Create Nivelez.py

---
+++ Juego/Módulos/Nivelez.py
@@ -0,0 +1,22 @@
+def nivel1():
+    minimo = 0
+    maximo = 99
+    return minimo, maximo
+
+def nivel2():
+    minimo = 0
+    maximo = 1000
+    return minimo, maximo
+
+def nivel3():
+    minimo = 0
+    maximo = 1000000
+    return minimo, maximo
+
+def nivel4():
+    minimo = 0
+    maximo = 1000000000000
+    return minimo, maximo
+
+def elegirnivel():
+    input
