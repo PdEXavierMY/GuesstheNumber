d6f0068111b3d8848c847cf600464a195debb0e3
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-13 20:46:02 +0100

    Update __init__.py

--- Juego/Módulos/__init__.py
+++ Juego/Módulos/__init__.py
@@ -1,3 +1,3 @@
-from .pedir_números import (pedir_entrada_numero, pedir_entrada_numero_delimitado)
+from .pedir_números import (pedir_entrada_numero, pedir_entrada_numero_delimitado, pedir_entrada_numero_delimitado_sinayuda, ayuda)
 
 from .comparar_respuestas import (pedir_entrada_si_o_no, pedir_entrada_verdadero_o_falso)
