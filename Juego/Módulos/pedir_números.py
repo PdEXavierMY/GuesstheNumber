254b2f03fdff22865e8bf3a034acbbca18117ca7
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-13 20:46:10 +0100

    Update pedir_números.py

--- Juego/Módulos/pedir_números.py
+++ Juego/Módulos/pedir_números.py
@@ -17,18 +17,18 @@ def pedir_entrada_numero(invitacion):
             return entrada
 
 def ayuda():
-    ayudas = pedir_entrada_verdadero_o_falso("¿Quieres recibir una ayuda extra durante el juego?(Se te irá actualizando el número mínimo y máximo entre el que está el número generado de entre los que vayas fallando)")
+    ayudas = pedir_entrada_verdadero_o_falso("¿Quieres recibir una ayuda extra durante el juego?(Se te irá actualizando el número mínimo y máximo entre el que está el número generado de entre los que vayas fallando) ")
     return ayudas
 
 def pedir_entrada_numero_delimitado(invitacion, minimo=MIN, maximo=MAX):
-    if ayuda() == True:
-        while True:
-            entrada = pedir_entrada_numero(invitacion)
-            if minimo <= entrada <= maximo:
-                return entrada
-    else:
-        while True:
-            invitacion = "{} entre {} y {} incluidos".format(invitacion, minimo, maximo)
-            entrada = pedir_entrada_numero(invitacion)
-            if minimo <= entrada <= maximo:
-                return entrada
+    while True:
+        invitacion = "{} entre {} y {} incluidos".format(invitacion, minimo, maximo)
+        entrada = pedir_entrada_numero(invitacion)
+        if minimo <= entrada <= maximo:
+            return entrada
+
+def pedir_entrada_numero_delimitado_sinayuda(invitacion, minimo=MIN, maximo=MAX):
+    while True:
+        entrada = pedir_entrada_numero(invitacion)
+        if minimo <= entrada <= maximo:
+            return entrada
